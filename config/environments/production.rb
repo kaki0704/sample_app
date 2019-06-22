8721f74d4d45444c6b9e4b42f8bbee36ecc98f14
yamady0704 <fleis_preis@yahoo.co.jp>
2019-06-23 04:41:01 +0900

    Add account activation3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app134035132@heroku.com'],
-    :password       => ENV['uprqaiku2912'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
